5f6050814a777e99d6b4d4edc1210c4d6a2eb9a3
mahade <owork.hasan@gmail.com>
2026-06-03 09:18:29 +0600

    commited

---
+++ reversing_an_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("Number of elements: %d\n", n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+    }
+
+    printf("\nReversed array: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
